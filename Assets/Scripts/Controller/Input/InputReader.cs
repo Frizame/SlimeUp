ad7a0b8e16785ef0533443fbb1c6b829c5913d7b
Eli Beber <ebeber318@gmail.com>
2026-06-15 01:36:20 -0400

    Update InputReader.cs

--- Assets/Scripts/Controller/Input/InputReader.cs
+++ Assets/Scripts/Controller/Input/InputReader.cs
@@ -8,10 +8,19 @@ public interface IInputReader
     void EnablePlayerActions();
 }
 
-public class InputReader : MonoBehaviour, IInputReader, IPlayerActions
+public class InputReader : ScriptableObject, IInputReader, IPlayerActions
 {
     public event UnityAction InteractPressed = delegate { };
     public event UnityAction InteractReleased = delegate { };
+
+    public event UnityAction<Vector2> MoveChanged = delegate { };
+    public event UnityAction<Vector2> LookChanged = delegate { };
+
+    /// <summary>Latest movement axis value (WASD / left stick), range -1..1 per axis.</summary>
+    public Vector2 MoveInput { get; private set; }
+
+    /// <summary>Latest look delta (mouse / right stick) for this frame.</summary>
+    public Vector2 LookInput { get; private set; }
 
     public PlayerInputActions inputActions;
     public void EnablePlayerActions()
@@ -54,7 +63,11 @@ public class InputReader : MonoBehaviour, IInputReader, IPlayerActions
 
     #region Player Actions
 
-    public void OnMove(InputAction.CallbackContext context) { }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveInput = context.ReadValue<Vector2>();
+        MoveChanged.Invoke(MoveInput);
+    }
     public void OnAttack(InputAction.CallbackContext context) { }
     public void OnCrouch(InputAction.CallbackContext context) { }
     public void OnInteract(InputAction.CallbackContext context)
@@ -69,7 +82,11 @@ public class InputReader : MonoBehaviour, IInputReader, IPlayerActions
         }
     }
     public void OnJump(InputAction.CallbackContext context) { }
-    public void OnLook(InputAction.CallbackContext context) { }
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        LookInput = context.ReadValue<Vector2>();
+        LookChanged.Invoke(LookInput);
+    }
     public void OnNext(InputAction.CallbackContext context) { }
     public void OnPrevious(InputAction.CallbackContext context) { }
     public void OnSprint(InputAction.CallbackContext context) { }
